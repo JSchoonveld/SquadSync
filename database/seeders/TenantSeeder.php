4a941424becb0745b13909f50214a045ff9d3b46
Jesse Schoonveld <jesse.schoonveld@beeproger.com>
2022-12-29 17:04:56 +0100

    csfix

--- database/seeders/TenantSeeder.php
+++ database/seeders/TenantSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Tenant;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class TenantSeeder extends Seeder
@@ -19,8 +18,8 @@ class TenantSeeder extends Seeder
         $tenants = Tenant::factory()->count(20)->create();
 
         foreach ($tenants as $tenant) {
-            User::factory()->count(rand(5,10))->create([
-                'tenant_id' => $tenant->id
+            User::factory()->count(rand(5, 10))->create([
+                'tenant_id' => $tenant->id,
             ]);
         }
     }
