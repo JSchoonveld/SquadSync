e01d84d360072a16f1d825368d6820032e5ee84c
JSchoonveld <jesse.schoonveld@gmail.com>
2023-01-16 17:04:05 +0100

    switched to spatie permission package (#8)
    Co-authored-by: Jesse Schoonveld <jesse.schoonveld@beeproger.com>

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,10 +11,11 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, BelongsToTenant;
+    use HasApiTokens, HasFactory, Notifiable, BelongsToTenant, HasRoles;
 
     /**
      * The attributes that are mass assignable.
