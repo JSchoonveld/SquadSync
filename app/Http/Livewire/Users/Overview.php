1d5141033b2f25fd7dc5a4fa16907bd3cf780b7f
JSchoonveld <jesse.schoonveld@gmail.com>
2023-01-20 17:00:20 +0100

    Permissions (#10)
    * switched to spatie permission package
    
    * temp disabled validation, enabled mail websockets
    
    Co-authored-by: Jesse Schoonveld <jesse.schoonveld@beeproger.com>

--- app/Http/Livewire/Users/Overview.php
+++ app/Http/Livewire/Users/Overview.php
@@ -20,6 +20,7 @@ class Overview extends Component
 
     public function toggleAdminOptions()
     {
+    
         if (Auth::user()->can('manage tenant users')) {
             $this->adminOptions = true;
         }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,28 @@
 import './bootstrap';
 import Alpine from 'alpinejs'
+import Echo from "laravel-echo"
+import Pusher from 'pusher-js';
+
 window.Alpine = Alpine
 Alpine.start()
+
+window.Pusher = Pusher;
+
+let echo = new Echo({
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
+    wsHost: window.location.hostname,
+    wsPort: 6001,
+    forceTLS: false,
+    disableStats: true,
+    enabledTransports: ['ws', 'wss'],
+});
+
+echo.private('message-sent-private')
+    .listen('UserSendsMessage', (e) => {
+        let messageCountElement = document.getElementById('sidebarMessageCount');
+        let currentCount = parseInt(messageCountElement.innerHTML);
+        messageCountElement.innerHTML = (currentCount += 1).toString();
+
+    })
